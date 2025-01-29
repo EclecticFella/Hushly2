9dbc7580c8d9d0e21c935c00525980a49510f8b1
EclecticFella <hushlyadmin@protonmail.com>
2025-01-29 14:45:51 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Enables static export for GitHub Pages
-  basePath: "/Hushly", // Ensure this matches your repo name
+  output: "export", // Enables static export
+  basePath: "/Hushly2", // Matches your repository name
   images: {
-    unoptimized: true, // Disables Next.js image optimization for GitHub Pages compatibility
+    unoptimized: true, // Disables Next.js image optimization for compatibility
   },
-  trailingSlash: true, // Ensures proper linking on GitHub Pages
+  trailingSlash: true, // Ensures proper linking
 };
 
 module.exports = nextConfig;
